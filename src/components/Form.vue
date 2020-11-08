c178fa41682283ae20c38e3fbba8472b874555d3
nejdetkadir <nejdetkadir.550@gmail.com>
2020-11-08 17:37:08 +0300

    Import vuelidate

--- src/components/Form.vue
+++ src/components/Form.vue
@@ -1,33 +1,33 @@
 <template>
-  <form>
+  <form @submit.prevent="onsubmit()">
     <div class="form-group">
       <label for="name">Your name and surname</label>
-      <input type="text" class="form-control" id="name">
+      <input v-model="form.userName" type="text" class="form-control" id="name">
       <small style="display: none" class="form-text text-danger"></small>
     </div>
     <div class="form-group">
       <label for="email">Your email</label>
-      <input type="email" class="form-control" id="email">
+      <input v-model="form.userEmail" type="email" class="form-control" id="email">
       <small style="display: none" class="form-text text-danger"></small>
     </div>
     <div class="form-group">
       <label for="phone">Your phone number</label>
-      <input type="text" class="form-control" id="phone">
+      <input v-model="form.userPhone" type="text" class="form-control" id="phone">
       <small style="display: none" class="form-text text-danger"></small>
     </div>
     <div class="form-group">
       <label for="bio">About yourself</label>
-      <textarea class="form-control" id="bio" rows="3"></textarea>
+      <textarea v-model="form.userBio" class="form-control" id="bio" rows="3"></textarea>
       <small style="display: none" class="form-text text-danger"></small>
     </div>
     <div class="form-group">
       <label for="pass">Password</label>
-      <input type="password" class="form-control" id="pass">
+      <input v-model="form.userPassword" type="password" class="form-control" id="pass">
       <small style="display: none" class="form-text text-danger"></small>
     </div>
     <div class="form-group">
       <label for="repass">Password again</label>
-      <input type="password" class="form-control" id="repass">
+      <input v-model="form.userRePassword" type="password" class="form-control" id="repass">
       <small style="display: none" class="form-text text-danger"></small>
     </div>
     <button type="submit" class="btn btn-success btn-block">SIGN UP</button>
@@ -36,7 +36,24 @@
 
 <script>
   export default {
-    name: "Form"
+    name: "Form",
+    data() {
+      return{
+        form: {
+          userName: null,
+          userEmail: null,
+          userPhone: null,
+          userBio: null,
+          userPassword: null,
+          userRePassword: null
+        }
+      }
+    },
+    methods: {
+      onsubmit() {
+        alert('submitted!');
+      }
+    }
   }
 </script>
 
