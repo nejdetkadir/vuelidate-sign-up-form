a256d8d0bf0e28ca6fc71b9bbf1d9a7d9a158250
nejdetkadir <nejdetkadir.550@gmail.com>
2020-11-08 17:29:07 +0300

    Create component structure

--- src/App.vue
+++ src/App.vue
@@ -5,55 +5,26 @@
         <div class="card shadow-lg p-3 mb-5 bg-white rounded" style="width: 100%;">
           <div class="card-body">
             <div class="text-center mb-5">
-              <img src="../src/assets/logo.svg" alt="">
+              <img src="../src/assets/logo.svg">
             </div>
-            <form>
-              <div class="form-group">
-                <label for="name">Your name and surname</label>
-                <input type="text" class="form-control" id="name">
-                <small style="display: none" class="form-text text-danger"></small>
-              </div>
-              <div class="form-group">
-                <label for="email">Your email</label>
-                <input type="email" class="form-control" id="email">
-                <small style="display: none" class="form-text text-danger"></small>
-              </div>
-              <div class="form-group">
-                <label for="phone">Your phone number</label>
-                <input type="text" class="form-control" id="phone">
-                <small style="display: none" class="form-text text-danger"></small>
-              </div>
-              <div class="form-group">
-                <label for="bio">About yourself</label>
-                <textarea class="form-control" id="bio" rows="3"></textarea>
-                <small style="display: none" class="form-text text-danger"></small>
-              </div>
-              <div class="form-group">
-                <label for="pass">Password</label>
-                <input type="password" class="form-control" id="pass">
-                <small style="display: none" class="form-text text-danger"></small>
-              </div>
-              <div class="form-group">
-                <label for="repass">Password again</label>
-                <input type="password" class="form-control" id="repass">
-                <small style="display: none" class="form-text text-danger"></small>
-              </div>
-              <button type="submit" class="btn btn-success btn-block">SIGN UP</button>
-            </form>
+            <Form></Form>
           </div>
         </div>
-
       </div>
     </div>
   </div>
 </template>
 
 <script>
+  import Form from "@/components/Form";
+
   export default {
     name: 'App',
+    components: {
+      Form
+    }
   }
 </script>
 
 <style>
-
 </style>
---
+++ src/components/Form.vue
@@ -0,0 +1,45 @@
+<template>
+  <form>
+    <div class="form-group">
+      <label for="name">Your name and surname</label>
+      <input type="text" class="form-control" id="name">
+      <small style="display: none" class="form-text text-danger"></small>
+    </div>
+    <div class="form-group">
+      <label for="email">Your email</label>
+      <input type="email" class="form-control" id="email">
+      <small style="display: none" class="form-text text-danger"></small>
+    </div>
+    <div class="form-group">
+      <label for="phone">Your phone number</label>
+      <input type="text" class="form-control" id="phone">
+      <small style="display: none" class="form-text text-danger"></small>
+    </div>
+    <div class="form-group">
+      <label for="bio">About yourself</label>
+      <textarea class="form-control" id="bio" rows="3"></textarea>
+      <small style="display: none" class="form-text text-danger"></small>
+    </div>
+    <div class="form-group">
+      <label for="pass">Password</label>
+      <input type="password" class="form-control" id="pass">
+      <small style="display: none" class="form-text text-danger"></small>
+    </div>
+    <div class="form-group">
+      <label for="repass">Password again</label>
+      <input type="password" class="form-control" id="repass">
+      <small style="display: none" class="form-text text-danger"></small>
+    </div>
+    <button type="submit" class="btn btn-success btn-block">SIGN UP</button>
+  </form>
+</template>
+
+<script>
+  export default {
+    name: "Form"
+  }
+</script>
+
+<style scoped>
+
+</style>
